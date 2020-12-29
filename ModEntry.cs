a99998ad7ae87b3a06ffd5b5153e0c9362bb8fb9
Agnes Jang <agnesj@uci.edu>
2020-12-29 15:34:02 -0800

    attempt lol

--- ModEntry.cs
+++ ModEntry.cs
@@ -6,16 +6,16 @@ using StardewModdingAPI.Utilities;
 using StardewValley;
 using StatsdClient;
 using log4net;
-using System.Reflection;
-using log4net.Config;
+using System.Security.Permissions;
 
 namespace DdStardewValley
 {
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
+        private static ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //private static log4net.Repository.ILoggerRepository logRepo = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+        
         /*********
         ** Public methods
         *********/
@@ -23,7 +23,6 @@ namespace DdStardewValley
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            XmlConfigurator.Configure();
             helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         }
 
@@ -36,6 +35,11 @@ namespace DdStardewValley
         /// <param name="e">The event data.</param>
         private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
         {
+            //log4net.Config.XmlConfigurator.Configure(logRepo, new System.IO.FileInfo("log4net.config"));
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+            //FileIOPermission f2 = new FileIOPermission(FileIOPermissionAccess.Read, "~/dev/dd-stardew-valley");
+            //f2.AddPathList(FileIOPermissionAccess.Write | FileIOPermissionAccess.Read, "~/dev/dd-stardew-valley/bin/Debug/myapp.log");
+            this.Monitor.Log("configured logger", LogLevel.Debug);
             this.Monitor.Log("Save loaded", LogLevel.Debug);
             var dogstatsdConfig = new StatsdConfig
             {
@@ -47,7 +51,8 @@ namespace DdStardewValley
             DogStatsd.Counter("dev.stardew.save_loaded", 2, tags: new[] { "player:" + Game1.player.name });
             this.Monitor.Log("Metric sent", LogLevel.Debug);
             String testLogLine = String.Format("Save successfully loaded from player {0}", Game1.player.name);
-            _log.Info(testLogLine);
+            _log.Debug(testLogLine);
+            this.Monitor.Log("Sent log line", LogLevel.Debug);
 
             DogStatsd.Dispose(); // Flush all metrics not yet sent
         }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,6 +12,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+//[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+[log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"))]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
